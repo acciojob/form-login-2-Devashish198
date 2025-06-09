553fb8b0457e46069cb21bf85850f215cb0015f6
Devashish198 <dev.ashish.shukla281977@gmail.com>
2025-06-09 08:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("Submit").addEventListener("click",()=>{
+	First=document.getElementById("FirstName").value;
+	Last=document.getElementById("LastName").value;
+	PhoneNumber=document.getElementById("PhoneNumber").value;
+	Email=document.getElementById("Email").value;
+	alert(`First Name: ${First} Last Name: ${Last} Phone Number: ${PhoneNumber} Email ID: ${Email}`)
+
+	
+})
